52f25cae2eaa5fb5a2c14f28012e20154eb411d1
soumen8890 <130748304+soumen8890@users.noreply.github.com>
2025-06-05 18:32:26 +0530

    Create database.py

---
+++ database/database.py
@@ -0,0 +1,24 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from config.settings import settings
+
+SQLALCHEMY_DATABASE_URL = settings.DB_URL
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True,
+    pool_size=20,
+    max_overflow=30
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
