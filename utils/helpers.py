cdf67276e11348b9a6632f86eaef5d1892ace4a5
soumen8890 <130748304+soumen8890@users.noreply.github.com>
2025-06-05 19:34:23 +0530

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,1 +1,7 @@
+import re
+from datetime import datetime, timedelta
+from typing import Optional, Union
 
+def validate_phone_number(phone: str) -> bool:
+    """Validate Indian phone numbers"""
+    return bool(re.match(r'^[6-9]\d{9}$
